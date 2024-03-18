f01e033cfe34e0a6bd5a72071ecb8b1ba3ea551b
Kelebogil <kele.ply@gmail.com>
2024-03-18 10:37:26 +0200

    adding my player

--- main.py
+++ main.py
@@ -10,14 +10,21 @@ pygame.display.set_caption("space Dodge")
 
 BG =pygame.transform.scale(pygame.image.load("bj.jpg"), (WIDTH,HEIGHT))
 
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
 
-def draw():
+def draw(player):
     WIN.blit(BG, (0, 0))
+
+    pygame.draw.rect(WIN, "red",player)
+
     pygame.display.update()
 
 
 def main():
     run= True
+
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT,PLAYER_WIDTH, PLAYER_HEIGHT)
 
     while run:
         for event in pygame.event.get():
@@ -25,7 +32,7 @@ def main():
                 run =False
                 break
 
-            draw()
+            draw(player)
 
     pygame.quit()        
 

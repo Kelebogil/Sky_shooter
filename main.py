065f1c199996b1081deb52c62ea028af603b3f02
Kelebogil <kele.ply@gmail.com>
2024-03-20 10:41:41 +0200

    adding the time and cnstants for my player

--- main.py
+++ main.py
@@ -1,48 +1,61 @@
 import pygame 
 import time
 import random
+pygame.font.init()
 
-#creating a window
+# Creating a window
+WIDTH, HEIGHT = 1000, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Dodge")
 
-WIDTH,HEIGHT = 1000,600
-WIN =pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("space Dodge")
+# Loading background image
+BG = pygame.transform.scale(pygame.image.load("bj.jpg"), (WIDTH, HEIGHT))
 
-BG =pygame.transform.scale(pygame.image.load("bj.jpg"), (WIDTH,HEIGHT))
-
+# Constants for player
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
-
 PLAYER_VEL = 5
 
-def draw(player):
+FONT = pygame.font.SysFont("comicsans", 30) #writing the font for the time board
+
+def draw(player, elapsed_time):
     WIN.blit(BG, (0, 0))
 
-    pygame.draw.rect(WIN, "red",player)
+    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1,"White")
+    WIN.blit(time_text, (10,10))
+
+    pygame.draw.rect(WIN, "red", player)
 
     pygame.display.update()
 
-
 def main():
-    run= True
+    run = True
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, 
+                         PLAYER_WIDTH,PLAYER_HEIGHT)
+    clock =pygame.time.Clock()
 
-    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT,PLAYER_WIDTH, PLAYER_HEIGHT)
-
+    start_time = time.time()
+    elapsed_time =0
+                              
     while run:
+        clock.tick(60)
+        elapsed_time = time.time() - start_time
+
         for event in pygame.event.get():
-            if event.type== pygame.QUIT:
-                run =False
-                break
+            if event.type == pygame.QUIT:
+                run = False
 
-            keys = pygame. key.get_pressed
-            if keys[pygame.K_LEFT]:
-                player.x -= PLAYER_VEL
-            if keys[pygame.K_RIGHT]:
-                player.y -= PLAYER_VEL
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
+            player.x += PLAYER_VEL
 
-            draw(player)
+        draw(player, elapsed_time)
 
-    pygame.quit()        
+    pygame.quit()
 
 if __name__ == "__main__":
-    main()                        
+    pygame.init()
+    main()
